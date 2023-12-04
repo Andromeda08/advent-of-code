58329687015a60666c060037dcf54052f57c459f
Andromeda08 <vkovacsbalazs26@gmail.com>
2023-12-04 14:01:23 +0100

    Day 4

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -2,32 +2,30 @@ import kotlin.math.pow
 
 data class Card(val id: Int, val key: List<Int>, val ans: List<Int>, val matches: Int)
 
-fun day4Part1(input: List<String>): Int {
-    return input.sumOf { card ->
+fun getCards(input: List<String>): List<Card> {
+    return input.map { card ->
         val (a, b) = card.drop(card.indexOfFirst { it == ':' } + 2).split(" | ")
-        val ans = a.windowed(2, 3).map { it.trim().toInt() }.toSet()
-        var res = -1
-        b.windowed(2, 3).map { it.trim().toInt() }.forEach { if (it in ans) res++ }
-        2.0.pow(res).toInt()
+        val key = a.windowed(2, 3).map { it.trim().toInt() }.toSet()
+        val ans = b.windowed(2, 3).map { it.trim().toInt() }
+        var matches = 0
+        ans.forEach { if (it in key) matches++ }
+        Card(card.drop(5).split(':')[0].trim().toInt(), key.toList(), ans, matches)
     }
 }
 
-fun getCards(input: List<String>): List<Card> {
-    return input.map { card ->
-        val (id, nums) = card.drop(5).split(':')
-        val (key, ans) = nums.split(" | ").map { str ->
-            str.trim().windowed(2, 3).map { it.trim().toInt() }
-        }
-        val matches = mutableListOf<Int>()
-        ans.forEach { if (it in key) matches.add(it) }
-        Card(id.trim().toInt(), key, ans, matches.count())
-    }
+fun day4Part1(input: List<String>): Int {
+    return getCards(input).sumOf { 2.0.pow(it.matches - 1).toInt() }
 }
 
 fun day4Part2(input: List<String>): Int {
-    val cards = getCards(input).forEach {println(it)}
-
-    return 0
+    val cards = getCards(input)
+    val instances = cards.associate { card -> card.id to 1 }.toMutableMap()
+    cards.forEach { card ->
+        for (i in (card.id + 1)..((card.id + card.matches))) {
+            instances[i] = (instances[i] ?: 1) + (instances[card.id] ?: 1)
+        }
+    }
+    return instances.values.reduce(Int::plus)
 }
 
 fun main() {
